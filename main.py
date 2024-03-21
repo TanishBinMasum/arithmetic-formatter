b3bb6d649a565bd67617a64b0576e30b4fb0e21e
Tanish Bin Masum <137204730+TanishBinMasum@users.noreply.github.com>
2024-03-21 18:49:21 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
+#this is the file that actually does the formatting
+
 from pytest import main
 
 from arithmetic_arranger import arithmetic_arranger
 
-
+#for a person who doesnt code this might look a little blursed but it's not much
 print(arithmetic_arranger(["32 + 698", "3801 - 2", "45 + 43", "123 + 49"]))
 
 
