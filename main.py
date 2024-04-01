763449fe8c38cfcbd15775a9091b4ee41a2fe531
Tanish Bin Masum <137204730+TanishBinMasum@users.noreply.github.com>
2024-04-01 10:20:40 +0600

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from pytest import main
 
 from arithmetic_arranger import arithmetic_arranger
 
-#for a person who doesnt code this might look a little blursed but it's not much
+#for a person who doesnt code this might look a little cursed but it's not much
 print(arithmetic_arranger(["32 + 698", "3801 - 2", "45 + 43", "123 + 49"]))
 
 
