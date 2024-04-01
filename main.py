b2f678bda60e5e5943b0759fa950f9b5d64aefae
Tanish Bin Masum <137204730+TanishBinMasum@users.noreply.github.com>
2024-04-01 10:22:04 +0600

    Update main.py

--- main.py
+++ main.py
@@ -7,5 +7,6 @@ from arithmetic_arranger import arithmetic_arranger
 #for a person who doesnt code this might look a little cursed but it's not much
 print(arithmetic_arranger(["32 + 698", "3801 - 2", "45 + 43", "123 + 49"]))
 
+
 # Run unit tests automatically
 main(['-vv'])
